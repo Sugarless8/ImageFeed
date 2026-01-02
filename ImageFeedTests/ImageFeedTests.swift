867d25ca2fd8beb2d6135f79c0a8466b98bd7476
d.s.pakhomov <flamy97@yandex.ru>
2026-01-02 22:33:30 +0400

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Дмитрий Пахомов on 02.01.2026.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
