c64fc1e23d487b4d411ee11776d8591d895cc9cc
d.s.pakhomov <flamy97@yandex.ru>
2026-03-22 18:34:13 +0400

    no message

---
+++ ImagesList/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Пахомов on 10.03.2026.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey: String = "nh4DNXyAOC_MFp9H9eBhEWOW5p-JrddCKWddu2KrRkk"
+    static let secretKey: String = "TRZ5SdP_aFLs2aajjdh0K4Oqj4nntKVZllP6L__XjIk"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let baseURL: String = "https://api.unsplash.com"
+    static let accessScope: String = "public+read_user+write_likes"
+}
